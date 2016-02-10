1481c243349c11a7d64896753082a0d6a659c474
Col Perks <wild.fauve@gmail.com>
2016-02-11 12:54:47 +1300

    lots

--- lib/subscription_rater.rb
+++ lib/subscription_rater.rb
@@ -1,5 +1,6 @@
 class SubscriptionRater
 =begin
+MODEL
 {"telemetry_device_id":"5c74fbe4-2084-4339-a3ef-23f8d4d7de55",
 "telemetry_channel_id":"f7ded8d1-32fd-46a4-bfb4-e5539b0448e5",
 "ended_at":"2015-09-11 13:59:59 UTC",
@@ -31,6 +32,7 @@ class SubscriptionRater
   end
 
 # #<struct Struct::RatedUse
+# Message from RatedUse
 # telemetry_device_id="5c74fbe4-2084-4339-a3ef-23f8d4d7de55",
 # telemetry_channel_id="f7ded8d1-32fd-46a4-bfb4-e5539b0448e5",
 # ended_at=2015-09-11 12:59:59 UTC,
@@ -42,6 +44,8 @@ class SubscriptionRater
 # day=2015-09-11 00:00:00 +0000,
 # op="inc",
 # op_value=#<BigDecimal:7fae79057f10,'0.0',9(18)>>
+# read_op: "dec",
+# read_op_value: #<BigDecimal:7fae79057f10,'0.0',9(18)>>
 
   def create_subscription_period(rated_use, model)
     #model = SubscriptionModel.new
@@ -60,11 +64,12 @@ class SubscriptionRater
   def update_subscription_period(rated_use, model)
     # need a way of checking that the inc/dec message hasn't been received (a set of start times for the day perhaps)
     if rate_change(rated_use)
-      # strange behaviour of cassandra driver leaves UTC times as NOT UTC
+      # TODO: strange behaviour of cassandra driver leaves UTC times as NOT UTC
       if model.period_check.to_a.map {|t| t.gmtime}.include? rated_use.started_at  # so if we already have the period_check
         # TODO: READ needs ro have the same semantics
         puts "Applied #{rated_use.op} as #{rated_use.op_value}"
-        rated_use.charge = determine_change(rated_use) # the inc/dec can be obeyed
+        rated_use.charge = determine_charge_change(rated_use) # the inc/dec can be obeyed
+        rated_use.read = determine_read_change(rated_use)
       end
     end
     update_params(model, rated_use)
@@ -82,9 +87,14 @@ class SubscriptionRater
     end
   end
 
-  def determine_change(rated_use)
+  def determine_charge_change(rated_use)
     rated_use.op == "inc" ? rated_use.op_value : rated_use.op_value * -1
   end
+
+  def determine_read_change(rated_use)
+    rated_use.read_op == "inc" ? rated_use.read_op_value : rated_use.read_op_value * -1
+  end
+
 
   def init_params(m, rated_use)
     set_common(m, rated_use)
